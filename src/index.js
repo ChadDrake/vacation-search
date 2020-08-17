e04bd9c16e849bfdbd92a7c5fccbd892d27021ec
Harry <harry@fumblebee.site>
2020-08-17 17:33:04 -0400

    afternoon changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import $ from 'jquery';
+
 const apiKey = '0rMt3f6Fn76sQenA16HY2B8H6y0wc0TJ3a6zhh9q';
 const url = 'https://developer.nps.gov/api/v1/parks?';
+
+//https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=0rMt3f6Fn76sQenA16HY2B8H6y0wc0TJ3a6zhh9q
+
 function getState() {
-    
     let state = $('input[name=state]').val();
     $('input[name=state]').val('');
-    state = state.toUpperCase();
-    let result = state.split(',');
+    //state = state.toUpperCase();
+    let result = state.split(' ');
+    console.log(result);
     return result;
 }
 
 function getMaxResult() {
     let maxResult = $('input[name=max-results]').val();
     $('input[name=max-results]').val('');
-     return maxResult;
+    return maxResult;
 }
+
+const options = {
+    headers: new Headers({
+    "X-Api-Key": apiKey,
+    "accept": "application/JSON"
+    })
+  };
 
 function returnResult(){
     let params =  {
@@ -22,6 +33,8 @@ function returnResult(){
         limit:getMaxResult(),
     };
     let paramString = $.param(params);
+    console.log(paramString);
+    console.log(`${url}${paramString}&api_key=${apiKey}`);
     fetch(`${url}${paramString}&api_key=${apiKey}`)
     .then(result => console.log(result));
 }
