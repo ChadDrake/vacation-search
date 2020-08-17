fd9ce0ebf8d8a0bd89e4cf62689cdb57aa72147b
Chad Drake <chaddrake1313@yahoo.com>
2020-08-17 17:48:02 -0500

    final submission

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ const url = 'https://developer.nps.gov/api/v1/parks?';
 function getState() {
     let state = $('input[name=state]').val();
     $('input[name=state]').val('');
-    //state = state.toUpperCase();
-    let result = state.split(' ');
-    console.log(result);
-    return result;
+    state = state.toUpperCase();
+    //let result = state.split(',');
+    console.log(state);
+    return state;
 }
 
 function getMaxResult() {
@@ -20,29 +20,53 @@ function getMaxResult() {
     return maxResult;
 }
 
-const options = {
-    headers: new Headers({
-    "X-Api-Key": apiKey,
-    "accept": "application/JSON"
-    })
-  };
+// const options = {
+//     headers: new Headers({
+//     "X-Api-Key": apiKey,
+//     "accept": "application/JSON"
+//     })
+//   };
 
 function returnResult(){
     let params =  {
         stateCode:getState(),
         limit:getMaxResult(),
     };
+    let name = [];
+    let description = [];
+    let webUrl = [];
     let paramString = $.param(params);
-    console.log(paramString);
-    console.log(`${url}${paramString}&api_key=${apiKey}`);
     fetch(`${url}${paramString}&api_key=${apiKey}`)
-    .then(result => console.log(result));
+    .then(result => result.json())
+    .then(resultJson => getParks(resultJson));
+}
+
+function getParks(result) {
+    let name =[];
+    let description=[];
+    let webUrl =[];
+    for(let i = 0; i < result.data.length; i++) {
+        name.push(result.data[i].fullName);
+        description.push(result.data[i].description);
+        webUrl.push(result.data[i].url);
+    } generateHtml(name, description, webUrl);
+}
+
+function generateHtml(name, description, webUrl) {
+    $('ul').html('');
+    for(let i = 0; i<name.length; i++){
+        $('ul').append(`<li>${name[i]}</li>
+        <li>${description[i]}</li>
+        <li><a href="${webUrl[i]}">${webUrl[i]}</a></li>`);
+    }
+    
 }
 
 function handleSubmit() {
     $('main').on('submit', function (e) {
         e.preventDefault();
         returnResult();
+        getParks();
     });
 }
 
